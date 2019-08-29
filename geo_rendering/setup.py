01ffc1518e386be8c4cf06d2f95f5c5b30afc7dd
Alexina <mozart38@gmail.com>
2019-08-29 13:42:57 +0200

    doc: added LICENCE.txt and setup.py files

--- geo_rendering/setup.py
+++ geo_rendering/setup.py
@@ -1,30 +1,28 @@
-import pathlib
 from setuptools import setup
 
-# The directory containing this file
-HERE = pathlib.Path(__file__).parent
-
-# The text of the README file
-README = (HERE / "readme.md").read_text()
-
-# This call to setup() does all the work
 setup(
     name="geo_rendering",
     version="1.0.0",
     description="Rendering of images from a shapefile using OpenCV",
-    long_description=README,
+    long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     url="https://github.com/acoullandreau/geo_rendering_package",
     author="Alexina Coullandreau",
     license="MIT",
     classifiers=[
+        "Development Status :: 4 - Beta",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
+        "Intended Audience :: Developers",
+        "Topic :: Multimedia :: Graphics",
+        "Topic :: Multimedia :: Graphics :: Viewers",
+        "Topic :: Scientific/Engineering :: Visualization"
     ],
-    packages=["geo_rendering"],
+    packages=['geo_rendering', 'geo_rendering.tests'],
+    install_requires=["mock==3.0.5", "numpy==1.17.0", "pandas==0.25.1", "pyshp==2.1.0", "opencv-python==4.1.0.25"],
     include_package_data=True,
-    install_requires=["mock", "numpy", "pandas", "pyshp", "opencv-python"],
     test_suite='nose.collector',
-    tests_require=['nose']
+    tests_require=['nose'],
+    zip_safe=False
 )
